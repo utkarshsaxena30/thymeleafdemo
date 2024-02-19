9c8a9257a19b5a5238940c8755ebfd3560126460
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2024-02-19 19:58:35 +0530

    Select box

--- src/main/java/com/example/btimfl/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/example/btimfl/thymeleafdemo/controller/StudentController.java
@@ -1,18 +1,24 @@
 package com.example.btimfl.thymeleafdemo.controller;
 
 import com.example.btimfl.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/show-student-form")
     public String showStudentForm(Model theModel) {
         theModel.addAttribute("student", new Student());
+        theModel.addAttribute("countries", countries);
         return "student-form";
     }
 
--- src/main/java/com/example/btimfl/thymeleafdemo/model/Student.java
+++ src/main/java/com/example/btimfl/thymeleafdemo/model/Student.java
@@ -3,6 +3,7 @@ package com.example.btimfl.thymeleafdemo.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
     public Student() {}
 
@@ -25,6 +26,14 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     @Override
